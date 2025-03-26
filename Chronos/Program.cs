1cee49010471bc73d230d57c0b03f997395bec2c
Arthur Santos <arthurluiz.developer@gmail.com>
2025-03-26 14:22:06 +0000

    Declaracao e inicializacao de variaveis e inicializacao da contagem.

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Chronos
 {
@@ -6,7 +8,23 @@ namespace Chronos
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+
+            Thread.Sleep(500);
+
+            crono2.Start();
+
+            Thread.Sleep(250);
+
+            crono1.Stop();
+            crono2.Stop();
+
+
+        
         }
     }
 }
