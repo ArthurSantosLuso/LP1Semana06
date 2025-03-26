011d37b7026f104526f2f95103691260bafce747
Arthur Santos <arthurluiz.developer@gmail.com>
2025-03-26 23:45:55 +0000

    Teste dos metodos PickupPowerUp() e TakeDamage() adicionados ao projeto.

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -19,6 +19,24 @@ namespace MyGame
                 Console.WriteLine($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}");
             }
 
+            // Give 40 shield to the enemy.
+            enemies[0].PickupPowerUp(PowerUp.Shield, 40f);
+
+            Console.WriteLine($"{enemies[0].GetName()} {enemies[0].GetHealth()} {enemies[0].GetShield()}");
+
+            // Deal 50 damage to the enemy
+            enemies[0].TakeDamage(50f);
+
+            Console.WriteLine($"{enemies[0].GetName()} {enemies[0].GetHealth()} {enemies[0].GetShield()}");
+
+            // Heal 50 life to the enemy.
+            enemies[0].PickupPowerUp(PowerUp.Health, 50f);
+
+            Console.WriteLine($"{enemies[0].GetName()} {enemies[0].GetHealth()} {enemies[0].GetShield()}");
+
+
+
+
         }
     }
 }
