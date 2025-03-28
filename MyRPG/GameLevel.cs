505eb491e85ca6b3aa10a25381d9b29f67402c29
Arthur Santos <arthurluiz.developer@gmail.com>
2025-03-28 14:10:27 +0000

    Cosntructor e metodo SetEnemyInRoom() adicionado ao projeto.

--- MyRPG/GameLevel.cs
+++ MyRPG/GameLevel.cs
@@ -6,6 +6,22 @@ namespace MyRPG
 {
     public class GameLevel
     {
- 
+        private Enemy[] rooms;
+        private Hardness difficulty;
+
+        public GameLevel(int numberOfRooms, Hardness difficulty)
+        {
+            rooms = new Enemy[numberOfRooms];
+            this.difficulty = difficulty;
+        }
+
+        public void SetEnemyInRoom(int roomIndex, Enemy enemy)
+        {
+            if (roomIndex < 0 || roomIndex >= rooms.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(roomIndex), "Room index out of bounds");
+            }
+            rooms[roomIndex] = enemy;
+        }
     }
 }
